97f4b2e5ffe3b2643c98822987d841103c5cfd90
Nishil Shailesh Pathak <125343865+nishil61@users.noreply.github.com>
2025-07-03 09:49:18 +0000

    feat: create basic react app structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Gujarat Apollo Parts Identifier</h1>
+      <p>Application is loading...</p>
+    </div>
+  );
+}
+
+export default App;
